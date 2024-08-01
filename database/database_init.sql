0b4a83061896f21e2aa7181923d873168bc0b62a
ALazyGuy <razorprogrammer@gmail.com>
2024-08-01 15:20:27 +0300

    feat: Implemented containers for backend and database

--- database/database_init.sql
+++ database/database_init.sql
@@ -1,3 +1,19 @@
+create table accounts(
+                         id bigserial primary key,
+                         money int,
+                         is_active bool
+);
+
+create table users(
+    id bigserial primary key,
+    username varchar(20) not null unique,
+    password varchar(100) not null,
+    role varchar(10) not null,
+    account_id bigint,
+    constraint fk_account_id
+        foreign key(account_id) references accounts(id)
+);
+
 INSERT INTO accounts (id, money, is_active) VALUES (1, 0, true);
 INSERT INTO accounts (id, money, is_active) VALUES (2, 150, false);
 
